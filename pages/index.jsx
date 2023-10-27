1db0ee910801511f98bf77be608814abcee328c7
Joshua Morgan <jmorg0605@gmail.com>
2023-10-26 21:47:19 -0400

    created navbar unit for pages
    The header contains navigation bar for each page of the application. Currently it doesn't hide at minimum width.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,7 @@
 import { Routes } from "@config/routes";
 import styles from "./index.module.scss";
+import Link from "next/link";
+import { Button } from "@features/ui";
 
 const IssuesPage = () => {
   return (
@@ -7,6 +9,28 @@ const IssuesPage = () => {
       <header className={styles.header}>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/icons/logo-large.svg" alt="Prolog logo" />
+        <nav className={styles.navbar}>
+          <Button>
+            <Link className={styles.buttonNavbar} href={"/"}>
+              Home
+            </Link>
+          </Button>
+          <Button>
+            <Link className={styles.buttonNavbar} href={"/documentation"}>
+              documentation
+            </Link>
+          </Button>
+          <Button>
+            <Link className={styles.buttonNavbar} href={"/pricing"}>
+              pricing
+            </Link>
+          </Button>
+          <Button>
+            <Link className={styles.buttonNavbar} href={"/products"}>
+              products
+            </Link>
+          </Button>
+        </nav>
         <a href={Routes.projects}>Dashboard</a>
       </header>
       <button
