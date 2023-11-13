54a2d21f41b5dc03e9d2ab384f1affa3ccaa544c
Joshua Morgan <jmorg0605@gmail.com>
2023-11-13 12:11:40 -0500

    created storybook component

---
+++ features/ui/button/button.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Button, ButtonSize, ButtonColor } from "./button";
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  title: "UI/Button",
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {
+  args: {
+    size: ButtonSize.sm,
+    color: ButtonColor.primary,
+    children: "Button CTO",
+  },
+  parameters: {
+    pseudo: { focus: true, hover: true },
+  },
+};
--- features/ui/button/button.tsx
+++ features/ui/button/button.tsx
@@ -2,8 +2,51 @@ import { ButtonHTMLAttributes } from "react";
 import classNames from "classnames";
 import styles from "./button.module.scss";
 
-export function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
+export enum ButtonSize {
+  sm = "sm",
+  md = "md",
+  lg = "lg",
+  xl = "xl",
+}
+
+export enum ButtonColor {
+  primary = "primary",
+  secondary = "secondary",
+  gray = "gray",
+  error = "error",
+  warning = "warning",
+  success = "success",
+  empty = "empty",
+  emptyGray = "emptyGray",
+}
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  size?: ButtonSize;
+  color?: ButtonColor;
+  disabled?: boolean;
+}
+
+export function Button({
+  size = ButtonSize.sm,
+  color = ButtonColor.primary,
+  disabled = false,
+  children,
+  ...props
+}: ButtonProps) {
+  console.log({ props });
+
   return (
-    <button {...props} className={classNames(styles.button, props.className)} />
+    <button
+      {...props}
+      className={classNames(
+        styles.button,
+        styles[size],
+        styles[color],
+        props.className,
+      )}
+      disabled={disabled}
+    >
+      {children}
+    </button>
   );
 }
