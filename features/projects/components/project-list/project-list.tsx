fc039c61571ef141115f6224d3da511a7c37321f
Joshua Morgan <jmorg0605@gmail.com>
2023-11-22 08:59:29 -0500

    added alert component for request fails

--- features/projects/components/project-list/project-list.tsx
+++ features/projects/components/project-list/project-list.tsx
@@ -1,17 +1,31 @@
 import { ProjectCard } from "../project-card";
 import { useGetProjects } from "../../api/use-get-projects";
+
+import { Alert } from "@features/ui";
 import styles from "./project-list.module.scss";
 
 export function ProjectList() {
-  const { data, isLoading, isError, error } = useGetProjects();
+  const { data, isLoading, isError } = useGetProjects();
 
   if (isLoading) {
     return <div>Loading</div>;
   }
 
+  /*
+  The error message is displayed when the request fails.  
+ 
+  'React Query will retry failed queries three times per default with exponential backoff'
+  
+*/
   if (isError) {
-    console.error(error);
-    return <div>Error: {error.message}</div>;
+    return (
+      <div>
+        <Alert
+          type="error"
+          message="There was a problem while loading the project data"
+        />
+      </div>
+    );
   }
 
   return (
