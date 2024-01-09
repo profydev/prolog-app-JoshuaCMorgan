54048d295d97d67426003b6734d59698021d3632
Joshua Morgan <jmorg0605@gmail.com>
2024-01-09 12:29:17 -0500

    add loading spinner

---
+++ features/ui/loading/loading.tsx
@@ -0,0 +1,14 @@
+import styles from "./loading.module.scss";
+
+export function Loading() {
+  return (
+    <div className={styles.container}>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        className={styles.loading}
+        src={"/icons/loading-circle.svg"}
+        alt="loading-circle"
+      />
+    </div>
+  );
+}
