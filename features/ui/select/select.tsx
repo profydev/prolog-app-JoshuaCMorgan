d362183915ffabaf2da0825c66087d94df30b42b
Joshua Morgan <jmorg0605@gmail.com>
2024-02-12 20:45:15 -0500

    selected value is show in input element

--- features/ui/select/select.tsx
+++ features/ui/select/select.tsx
@@ -1,5 +1,6 @@
+import classNames from "classnames";
 import styles from "./select.module.scss";
-import React from "react";
+import React, { useState } from "react";
 export interface SelectOption {
   name: string;
   id: number;
@@ -10,7 +11,13 @@ type SelectProps = {
   options: Array<SelectOption>;
   disabled?: boolean;
 };
-export function Select({ name, labelText, options }: SelectProps) {
+export function Select({ name, labelText, options, ...props }: SelectProps) {
+  const [selectedValue, setSelectedValue] = useState<SelectOption | null>(null);
+
+  function handleSelection(selection: SelectOption) {
+    setSelectedValue(selection);
+  }
+
   return (
     <div className={styles.selectContainer}>
       <div className={styles.selectionContainer}>
@@ -19,14 +26,26 @@ export function Select({ name, labelText, options }: SelectProps) {
         </label>
       </div>
       <div className={styles.inputContainer}>
-        <input type="text" className={styles.input} id={name} />
+        <input
+          {...props}
+          id={name}
+          className={classNames(styles.input)}
+          defaultValue={selectedValue?.name}
+          readOnly
+        />
       </div>
       <div className={styles.listContainer}>
         <ul className={styles.items}>
           {options.map((option, idx) => {
             const { name } = option;
             return (
-              <li key={idx} className={styles.listItem}>
+              <li
+                key={idx}
+                className={styles.listItem}
+                onClick={() => {
+                  handleSelection(option);
+                }}
+              >
                 <input type="text" name={name} id={name} hidden />
                 <label htmlFor={name}>{name}</label>
               </li>
