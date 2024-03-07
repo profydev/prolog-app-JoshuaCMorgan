f7fc4be1c450b400134315835f75b25061ce6bf4
Joshua Morgan <jmorg0605@gmail.com>
2024-03-07 12:57:35 -0500

    refactor link to open issues page with correct filters

--- features/projects/components/project-card/project-card.tsx
+++ features/projects/components/project-card/project-card.tsx
@@ -63,7 +63,10 @@ export function ProjectCard({ project }: ProjectCardProps) {
         </div>
       </div>
       <div className={styles.bottomContainer}>
-        <Link href={Routes.issues} className={styles.viewIssuesAnchor}>
+        <Link
+          href={`${Routes.issues}/?project=${name}`}
+          className={styles.viewIssuesAnchor}
+        >
           View issues
         </Link>
       </div>
