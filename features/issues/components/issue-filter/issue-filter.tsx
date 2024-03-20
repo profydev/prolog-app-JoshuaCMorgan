1c0f364b671287e92dd6c92d96635d83fb7f38e2
Joshua Morgan <jmorg0605@gmail.com>
2024-03-20 12:53:03 -0400

    removed button since it is a future task

--- features/issues/components/issue-filter/issue-filter.tsx
+++ features/issues/components/issue-filter/issue-filter.tsx
@@ -1,4 +1,4 @@
-import { Select, Button, ButtonSize, Input } from "@features/ui";
+import { Select, Input } from "@features/ui";
 import styles from "./issue-filter.module.scss";
 import { IssueLevel, IssueStatus } from "@api/issues.types";
 import { useFilter } from "./use-filter";
@@ -18,62 +18,38 @@ export function IssueFilter() {
   }
 
   return (
-    <div data-cy="issue-filter-container" className={styles.filterContainer}>
-      <div className={styles.filterContent}>
-        <Button size={ButtonSize.lg} className={styles.resolveBtn}>
-          <span className={styles.icon}>
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 20 20"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M16.6667 5L7.50001 14.1667L3.33334 10"
-                stroke="white"
-                stroke-width="1.67"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-          </span>
-          Resolve selected issues
-        </Button>
-        <div className={styles.filters} data-cy="issue-filters">
-          <Select
-            className={styles.select}
-            options={statusOptions}
-            handleChange={(option) => {
-              handleFilterChange({
-                status: option?.value as IssueStatus | undefined,
-              });
-            }}
-            value={filters.status}
-            placeholder="Status"
-          />
-          <Select
-            className={styles.select}
-            options={errorOptions}
-            handleChange={(option) => {
-              handleFilterChange({
-                level: option?.value as IssueLevel | undefined,
-              });
-            }}
-            value={filters.level}
-            placeholder="Level"
-          />
-          <Input
-            type="search"
-            className={styles.input}
-            placeholder="Project Name"
-            iconSrc={"/icons/hour-glass.svg"}
-            onChange={debounce((value: string | undefined) => {
-              handleFilterChange({ project: value });
-            })}
-          ></Input>
-        </div>
-      </div>
+    <div className={styles.filters} data-cy="issue-filters">
+      <Select
+        className={styles.select}
+        options={statusOptions}
+        handleChange={(option) => {
+          handleFilterChange({
+            status: option?.value as IssueStatus | undefined,
+          });
+        }}
+        value={filters.status}
+        placeholder="Status"
+      />
+      <Select
+        className={styles.select}
+        options={errorOptions}
+        handleChange={(option) => {
+          handleFilterChange({
+            level: option?.value as IssueLevel | undefined,
+          });
+        }}
+        value={filters.level}
+        placeholder="Level"
+      />
+      <Input
+        type="search"
+        className={styles.input}
+        placeholder="Project Name"
+        iconSrc={"/icons/hour-glass.svg"}
+        onChange={debounce((value: string | undefined) => {
+          handleFilterChange({ project: value });
+        })}
+      ></Input>
     </div>
   );
 }
