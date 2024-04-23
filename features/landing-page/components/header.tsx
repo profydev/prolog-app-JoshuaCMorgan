0cd6831cf59ce3cf51327a6b7f2544fc40310f84
Joshua Morgan <jmorg0605@gmail.com>
2024-04-23 12:40:43 -0400

    add sidebar navigation and functionality

--- features/landing-page/components/header.tsx
+++ features/landing-page/components/header.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import styles from "./header.module.scss";
 import { Routes } from "@config/routes";
+import { Button } from "@features/ui";
+import { useState } from "react";
+import classNames from "classnames";
 
 const navLinks = [
   { text: "home", href: Routes.home },
@@ -9,24 +12,53 @@ const navLinks = [
   { text: "pricing", href: Routes.pricing },
 ];
 export function Header() {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   return (
     <header className={styles.header}>
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img src="/icons/logo-large.svg" alt="Prolog logo" />
-      <nav>
+      <div
+        className={classNames(
+          styles.menuOverlay,
+          isMobileMenuOpen && styles.isMobileMenuOpen,
+        )}
+      ></div>
+      <nav
+        className={classNames(
+          styles.nav,
+          isMobileMenuOpen && styles.isMobileMenuOpen,
+        )}
+      >
         <ul className={styles.linkList}>
-          {navLinks.map((link, index) => {
+          {navLinks.map((link) => {
             return (
-              <li key={index}>
+              <li key={link.text}>
                 <Link href={link.href}>{link.text}</Link>
               </li>
             );
           })}
         </ul>
+        <Link className={styles.dashboardButton} href={Routes.projects}>
+          Open Dashboard
+        </Link>
       </nav>
-      <Link className={styles.dashboardButton} href={Routes.projects}>
-        Open Dashboard
-      </Link>
+      <Button
+        onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
+        className={styles.menuButton}
+        unstyled={true}
+      >
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src={
+            isMobileMenuOpen
+              ? "/icons/close-landing.svg"
+              : "/icons/menu-landing.svg"
+          }
+          alt={isMobileMenuOpen ? "close menu" : "open menu"}
+          className={styles.menuIcon}
+        />
+      </Button>
     </header>
   );
 }
